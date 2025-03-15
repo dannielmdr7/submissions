50916e29c301bb8ba76db0683fcc5a6398cf1dc4
Daniel Montenegro <dannielmdr7@gmail.com>
2025-03-15 11:43:20 -0500

    feature(nav-item): component created and first maquetation ready

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
+import { NavItemComponent } from '../nav-item/nav-item.component';
 
-interface NavBarItem{
-  name:string;
-  icon:string;
+interface NavBarItem {
+  name: string;
+  icon: string;
 }
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [NavItemComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
   title = 'ZenduForms';
   navBarOptions: NavBarItem[] = [
-    {name: 'Forms', icon: ''},
-    {name: 'Customers', icon: ''},
-    {name: 'Submissions', icon: ''},
-    {name: 'History', icon: ''},
-    {name: 'Reports', icon: ''},
-    {name: 'Workflow', icon: ''}
+    { name: 'Forms', icon: '' },
+    { name: 'Customers', icon: '' },
+    { name: 'Submissions', icon: '' },
+    { name: 'History', icon: '' },
+    { name: 'Reports', icon: '' },
+    { name: 'Workflow', icon: '' }
   ]
 
 }
---
+++ src/app/components/nav-item/nav-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-item',
+  imports: [],
+  templateUrl: './nav-item.component.html',
+  styleUrl: './nav-item.component.scss'
+})
+export class NavItemComponent {
+  @Input({ required: true }) name: string = '';
+
+}
