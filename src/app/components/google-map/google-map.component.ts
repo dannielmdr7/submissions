8c1a739352493109cdc4b86a446282d7bd3be463
Daniel Montenegro <dannielmdr7@gmail.com>
2025-03-17 09:52:39 -0500

    feature(google-map): markers in map ready

--- src/app/components/google-map/google-map.component.ts
+++ src/app/components/google-map/google-map.component.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import { GoogleMap, MapAdvancedMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-google-map',
-  imports: [GoogleMap],
+  imports: [GoogleMap, MapAdvancedMarker],
   templateUrl: './google-map.component.html',
   styleUrl: './google-map.component.scss'
 })
 export class GoogleMapComponent {
+  locations: google.maps.LatLngLiteral[] = [
+    { lat: 4.6482784, lng: -74.272619 },
+    { lat: 4.1, lng: -74.8 },
+  ]
   center = signal<google.maps.LatLngLiteral>({ lat: 4.6482784, lng: -74.272619 });
   zoom = signal(7);
 }
